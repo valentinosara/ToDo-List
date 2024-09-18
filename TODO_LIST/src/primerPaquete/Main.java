9309727016361d1323f6500a164c0c6a1c2fc9ab
Santiago Troiano <119124159+SantiagoTroiano@users.noreply.github.com>
2024-09-18 12:45:35 -0300

    Agregué un syso a modo de prueba.

--- TODO_LIST/src/primerPaquete/Main.java
+++ TODO_LIST/src/primerPaquete/Main.java
@@ -5,6 +5,7 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("primer commit _ prueba");
+		System.out.println("Prueba de segundo commit de Santi");
 	}
 
 }
